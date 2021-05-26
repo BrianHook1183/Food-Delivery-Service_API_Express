5916eefafeac0e919d2e0b5c02735f8c049060ca
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-26 16:08:36 -0500

    basic response from method

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,5 +6,25 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /dishes handlers needed to make the tests pass
-//  add handlers and middleware functions to create, read, update, and list dishes. Note that dishes cannot be deleted.
+function list(req, res) {
+  res.json("list");
+}
+
+function create(req, res) {
+  res.json("create");
+}
+
+function read(req, res) {
+  res.json("read");
+}
+
+function update(req, res) {
+  res.json("update");
+}
+
+module.exports = {
+  list,
+  create,
+  read,
+  update,
+};
