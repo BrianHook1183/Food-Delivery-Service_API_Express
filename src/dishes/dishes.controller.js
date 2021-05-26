b7d725ff17ce089f5211932ce834e64de2c2995a
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-26 18:47:01 -0500

    POST /dishes with validation

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,12 +6,59 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
+function bodyIsValid(req, res, next) {
+  const dish = req.body.data;
+  const { name, description, price, image_url } = dish;
+  if (!dish) {
+    return next({
+      status: 400,
+      message: "Dish is empty",
+    });
+  }
+  if (!name) {
+    return next({
+      status: 400,
+      message: "Dish must include a name",
+    });
+  }
+  if (!description) {
+    return next({
+      status: 400,
+      message: "Dish must include a description",
+    });
+  }
+  if (!Number.isInteger(price) || price < 0) {
+    return next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  }
+  if (!price) {
+    return next({
+      status: 400,
+      message: "Dish must include a price",
+    });
+  }
+  if (!image_url) {
+    return next({
+      status: 400,
+      message: "Dish must include a image_url",
+    });
+  }
+
+  const newDish = { ...dish, id: nextId() };
+  res.locals.newDish = newDish;
+  next();
+}
+
 function list(req, res) {
-  res.json("list");
+  res.json({ data: dishes });
 }
 
 function create(req, res) {
-  res.json("create");
+  const { newDish } = res.locals;
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
 }
 
 function read(req, res) {
@@ -19,12 +66,12 @@ function read(req, res) {
 }
 
 function update(req, res) {
-  res.json("update");
+  res.json(res.locals.newDish);
 }
 
 module.exports = {
   list,
-  create,
+  create: [bodyIsValid, create],
   read,
-  update,
+  update: [bodyIsValid, update],
 };
