bc6797d57ce1624d203e0777b3268cdaad2af1a5
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-26 16:27:09 -0500

    basic response from method

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -3,8 +3,29 @@ const path = require("path");
 // Use the existing order data
 const orders = require(path.resolve("src/data/orders-data"));
 
-// Use this function to assigh ID's when necessary
+// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
-// add handlers and middleware functions to create, read, update, delete, and list orders.
+function read(req, res) {
+  res.json("read");
+}
+function update(req, res) {
+  res.json("update");
+}
+function destroy(req, res) {
+  res.json("delete");
+}
+function list(req, res) {
+  res.json("list");
+}
+function create(req, res) {
+  res.json("create");
+}
+
+module.exports = {
+  read,
+  update,
+  destroy,
+  list,
+  create,
+};
