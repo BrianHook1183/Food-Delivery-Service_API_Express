d92fd1622a4390e170936fd68cfa083f00504482
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-31 04:04:36 -0500

    /orders POST and /orders/:orderId GET pass tests

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -8,15 +8,27 @@ const nextId = require("../utils/nextId");
 
 function orderIdExists(req, res, next) {
   const { orderId } = req.params;
-  const foundOrder = orders.find((order) => order.id == orderId);
-  if (foundOrder === undefined) {
+
+  const foundIndex = orders.findIndex((order) => order.id == orderId);
+  if (foundIndex >= 0) {
+    res.locals.foundOrder = { index: foundIndex, order: orders[foundIndex] };
+    next();
+  } else {
     return next({
       status: 404,
       message: `Order does not exist: ${orderId}.`,
     });
   }
-  res.locals.foundOrder = foundOrder;
-  next();
+
+  const order = req.body.data;
+  if (order) {
+    if (order.id && orderId !== order.id) {
+      return next({
+        status: 400,
+        message: `Order id does not match route id. Order: ${order.id}, Route: ${orderId}`,
+      });
+    }
+  }
 }
 
 function bodyIsValid(req, res, next) {
@@ -24,12 +36,12 @@ function bodyIsValid(req, res, next) {
   const order = req.body.data;
   const { deliverTo, mobileNumber, status, dishes } = order;
 
-  if (order.id && orderId !== order.id) {
-    return next({
-      status: 400,
-      message: `Order id does not match route id. Order: ${order.id}, Route: ${orderId}`,
-    });
-  }
+  // if (order.id && orderId !== order.id) {
+  //   return next({
+  //     status: 400,
+  //     message: `Order id does not match route id. Order: ${order.id}, Route: ${orderId}`,
+  //   });
+  // }
 
   if (!deliverTo) {
     return next({
@@ -37,30 +49,38 @@ function bodyIsValid(req, res, next) {
       message: "Order must include a deliverTo",
     });
   }
-  if (!mobileNumber ) {
+  if (!mobileNumber) {
     return next({
       status: 400,
       message: "Order must include a mobileNumber",
     });
   }
-  if (!dishes ) {
+  if (!dishes) {
     return next({
       status: 400,
       message: "Order must include a dish",
     });
   }
-  // if (!dishes is not an array or array is empty) {
-  //   return next({
-  //     status: 400,
-  //     message: "Order must include at least one dish",
-  //   });
-  // }
-  // if (quantity is missing || is zero or less || is not an integer ) {
-  //   return next({
-  //     status: 400,
-  //     message: "Dish ${index} must have a quantity that is an integer greater than 0",
-  //   });
-  // }
+  if (!Array.isArray(dishes) || dishes.length === 0) {
+    return next({
+      status: 400,
+      message: "Order must include at least one dish",
+    });
+  }
+
+  for (let i = 0; i < dishes.length; i++) {
+    const dish = dishes[i];
+    if (
+      !dish.quantity ||
+      !Number.isInteger(dish.quantity) ||
+      !dish.quantity > 0
+    ) {
+      return next({
+        status: 400,
+        message: `Dish ${i} must have a quantity that is an integer greater than 0`,
+      });
+    }
+  }
 
   // Adds new id for creating order but doesn't if updating and id not in body
   let handleId = undefined;
@@ -69,15 +89,14 @@ function bodyIsValid(req, res, next) {
   } else {
     handleId = orderId ? { id: orderId } : { id: nextId() };
   }
-//TODO need to adjust this for having dishes array embedded - and says "Note: Each dish in the Order's dishes property is a complete copy of the dish, rather than a reference to the dish by id. This is so the order does not change retroactively if the dish data is updated some time after the order is created."
+  //TODO need to adjust this for having dishes array embedded - and says "Note: Each dish in the Order's dishes property is a complete copy of the dish, rather than a reference to the dish by id. This is so the order does not change retroactively if the dish data is updated some time after the order is created."
   const newOrder = { ...order, ...handleId };
   res.locals.newOrder = newOrder;
   next();
-
 }
 
 function read(req, res) {
-  res.json({ data: res.locals.foundOrder });
+  res.json({ data: res.locals.foundOrder.order });
 }
 function update(req, res) {
   res.json("update");
@@ -90,7 +109,7 @@ function list(req, res) {
 }
 function create(req, res) {
   const { newOrder } = res.locals;
-  dishes.push(newOrder);
+  orders.push(newOrder);
   res.status(201).json({ data: newOrder });
 }
 
